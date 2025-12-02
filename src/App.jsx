d35fafba7f913a152fbc1fb15a17b8b5a9f61a60
Erlantz Caballero García <erlantzcg89@gmail.com>
2025-12-02 09:05:14 +0100

    useState & useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return 'Hola Mundo'
+import { useState, useEffect} from 'react';
+import * as API from './services/launches';
+
+export function App() {
+
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches);
+  }, []);
+
+  return (
+    <>
+      <h1>SpaceX Launches</h1>
+        <ul>
+          {launches.map(launch => (
+            <li key={launch.flight_number}>
+              <strong>{launch.mission_name}</strong> ({launch.launch_year}) - {launch.launch_success ? 'Success' : 'Failure'}
+            </li>
+          ))}
+        </ul>
+    </>
+  );
 }
 
 export default App
